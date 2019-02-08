1db6a3a950418a1a66ba9fd89751418c588bc455
mdr-leon10 <mdr.leon10@uniandes.edu.co>
2019-02-08 10:08:40 -0500

    Update import csv.py

--- Scripts pequeños/import csv.py
+++ Scripts pequeños/import csv.py
@@ -1,16 +1,19 @@
+#!/usr/bin/env python3
 import csv
 cliente = ''
 
-with open ('prueba.csv', 'rb') as csvfile:
+
+with open ('prueba.csv', 'r') as csvfile:
 	with open ('resultadoPrueba.csv', 'w') as csvWrite:
 		writer = csv.writer(csvWrite)
 		prueba = csv.reader(csvfile)
 		for row in prueba:
-			split = row[0].split('//')
-			if (split[len(split)-1].find(' - ') != -1):
-				cliente = str(split[len(split)-1])
-			else:
-				cliente = '\n' 
+			print(str(len(row)))
+			if(len(row)>= 1):
+				split = row[0].split('//')
+				if (len(split)>1 and split[len(split)-1].find(' - ') != -1):
+					cliente = split[len(split)-1]
+				else:
+					cliente = 'no hay'  
 			writer.writerow([cliente])
-
 		
